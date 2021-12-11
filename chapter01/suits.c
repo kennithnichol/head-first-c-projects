0201ad14a19074f0fd7cf31cdea1c681387badbf
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2021-12-11 15:09:16 -0800

    add chapter01

---
+++ chapter01/suits.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int suit = 'H';
+	
+	switch(suit) {
+	case 'C':
+		puts("Clubs");
+		break;
+	case 'D':
+		puts("Diamonds");
+		break;
+	case 'H':
+		puts("Hearts");
+		break;
+	default:
+		puts("Spades");
+	}
+
+	return 0;
+}
